3149ac282ca1d8beae3a8ec99704e17dc842219c
Wechuli <wechulipaul@yahoo.com>
2021-03-08 14:53:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const data = require("./github/workflow/event.json");
+const fs = require('fs');
 
-console.log(data);
+
+// const data = require("./github/workflow/event.json");
+
+const event = JSON.parse(fs.readFileSync('/github/workflow/event.json', 'utf8'))
+
+console.log(event);
